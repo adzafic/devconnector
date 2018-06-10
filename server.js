625d976b5ee400f2b36e05aceda53eb8609fbfc7
adzafic <adnan.dzafic21@gmail.com>
2018-06-10 19:52:58 +0200

    conectar a base de datos

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB CONFIG
+const db = require("./config/keys").mongoURI;
+
+// CONNECT TO MONGODB
+
+mongoose
+  .connect(db)
+  .then(() => console.log("mongo db conectado"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("hola"));
 
