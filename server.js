a512e1d44cbc45998523f9c1cc26cfe14b948418
adzafic <adnan.dzafic21@gmail.com>
2018-06-17 13:05:33 +0200

    añadir registro de usuarios

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParse = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body parser midelware
+app.use(bodyParse.urlencoded({ extended: false }));
+app.use(bodyParse.json());
 
 // DB CONFIG
 const db = require("./config/keys").mongoURI;
