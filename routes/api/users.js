e2c920d7bd1297d2e2a4c1cf1d4a253bf030a9ea
adzafic <adnan.dzafic21@gmail.com>
2018-06-17 16:16:19 +0200

    crear token y añadir estrategias de passport jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const keys = require("../../config/keys");
 const User = require("../../models/User");
 
 // @route   GET api/users/test
@@ -12,7 +14,7 @@ router.get("/test", (req, res) => {
   res.json({ msg: "Usuarios funciona" });
 });
 
-// @route   GET api/users/register
+// @route   POST api/users/register
 // @desc    registrar el usuario en la base de datos
 // @acces   Public
 router.post("/register", (req, res) => {
@@ -46,4 +48,60 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route   POST api/users/login
+// @desc    LOGIN usuario devuelve token
+// @acces   Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //ecnontrar usuario por email
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "Usuario no existe" });
+    }
+    //comprobar contraseña
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        //usuario coincide
+        // crear jwt payload
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        };
+        //logear token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              succes: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        res.status(400).json({ password: "Contraseña incorrecta" });
+      }
+    });
+  });
+});
+
+// @route   GET api/users/current
+// @desc    devuelve el usuario actual
+// @acces   private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParse = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -22,7 +23,11 @@ mongoose
   .then(() => console.log("mongo db conectado"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hola"));
+//Passpaort midelware
+app.use(passport.initialize());
+
+//Passport config
+require("./config/passport")(passport);
 
 //Usar rutas
 app.use("/api/users", users);
