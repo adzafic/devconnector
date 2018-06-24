700064ef6f8bdb15e5e2b0d92abeddd0cf41f703
adzafic <adnan.dzafic21@gmail.com>
2018-06-24 15:52:08 +0200

    Añadir la ruta del perfil actual

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//cargar modelos
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // @route   GET api/perfile/test
 // @desc    Test Perfil ruta
@@ -7,5 +13,26 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({ msg: "Perfil funciona" });
 });
+
+// @route   GET api/perfile
+// @desc    Perfil actual del usuario
+// @acces   Private
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "No hay perfil para este usuario";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
